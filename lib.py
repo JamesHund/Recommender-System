ee4967c9f75d76f658ed4412a087eea43970feac
James Hund <jameshundp@gmail.com>
2024-02-27 10:03:05 +0200

    Working version of recommender system.

--- lib.py
+++ lib.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 class Movie:
 
-    def __init__(self, movie_id, title = "", genres = [], ratings = []):
+    def __init__(self, movie_id, ratings, title = "", genres = []):
         self.movie_id = movie_id
         self.title = title
         self.genres = genres
@@ -14,14 +14,14 @@ class Movie:
 
 class User:
 
-    def __init__(self, uid, ratings = []):
+    def __init__(self, uid, ratings):
         self.uid = uid
         self.ratings = ratings
 
 
 class Recommender:
 
-    def __init__(self, k = 20, gamma=2, lam=2, tau=0.05):
+    def __init__(self, k = 20, gamma=0.02, lam=0.05, tau=0.05):
         self.uid_map = {}
         self.mid_map = {}
         self.movie_title_to_id = {}
@@ -52,7 +52,7 @@ class Recommender:
 
                 if movie_id not in self.mid_map:
                     self.mid_map[movie_id] = m_index
-                    self.movies.append(Movie(movie_id, ratings=[]))
+                    self.movies.append(Movie(movie_id, []))
                     m_index += 1
 
                 user_rating_tuple = (self.mid_map[movie_id], rating)
@@ -60,7 +60,7 @@ class Recommender:
                 #check if user has been created for user_id
                 if user_id not in self.uid_map:
                     self.uid_map[user_id] = u_index
-                    self.users.append(User(user_id, ratings=[user_rating_tuple]))
+                    self.users.append(User(user_id, [user_rating_tuple]))
                     u_index += 1
                 else:
                     self.users[self.uid_map[user_id]].ratings.append(user_rating_tuple)
@@ -92,6 +92,7 @@ class Recommender:
             user.ratings = np.array(user.ratings)
 
         for movie in tqdm(self.movies, "Reformatting movie ratings"):
+            assert len(movie.ratings) != 0
             movie.ratings = np.array(movie.ratings) 
             
     def neg_log_likelihood(self):
@@ -117,89 +118,14 @@ class Recommender:
     def RMSE(self):
         sum_rankings = 0.0
 
+        num_ratings = 0
         for m, user in zip(range(len(self.users)), self.users):
             for n, rank in user.ratings:
                 sum_rankings += (rank - self.U[m,:].dot(self.V[int(n),:]) - self.user_biases[m] - self.movie_biases[int(n)])**2
+                num_ratings += 1
         
-        return np.sqrt(sum_rankings)
-
-    def fit(self, max_iter = 100):
-        M = len(self.users)
-        N = len(self.movies)
-
-        #user biases
-        self.user_biases = np.zeros((M))
-        #movie biases
-        self.movie_biases = np.zeros((N))
-
-        #initialize U and V matrices
-        self.U = np.random.normal(0, 1 / np.sqrt(self.k), size=(M, self.k))
-        self.V = np.random.normal(0, 1 / np.sqrt(self.k), size=(N, self.k))
-
-        converged = False
-        it = 0
-
-        neg_log_likelihoods = []
-        RMSE = []
-
-        for i in tqdm(range(max_iter)):
-            #iterate through users
-            for m, user in zip(range(M), self.users):
-                #update user bias b_m(u)
-                rating_sum = 0.0
-                for n, rating in user.ratings: 
-                    rating_sum += rating - self.U[m,:].dot(self.V[n,:]) - self.movie_biases[n]
-                rating_sum *= self.lam
-                denom = self.gamma + self.lam*M
-                self.user_biases[m] = rating_sum/denom
-
-                #update user embedding u_m
-                rating_sum = np.zeros((self.k,1))
-                denom_matrix = np.zeros((self.k, self.k))
-                for n, rating in user.ratings:
-                    scalar = (rating - self.user_biases[m] - self.movie_biases[n])
-                    vector = scalar*self.V[n,:].reshape((self.k, 1))
-                    rating_sum = rating_sum + vector
-                    denom_matrix += self.V[n,:].dot(self.V[n,:])
-
-                rating_sum = rating_sum*self.lam
-                denom_matrix = denom_matrix*self.lam + self.tau*np.eye(self.k)
-                denom_matrix = np.linalg.inv(denom_matrix)
-
-                self.U[m,:] = denom_matrix.dot(rating_sum.reshape((self.k,1))).reshape((self.k,))
-
-            
-            #iterate through movies 
-            for n, movie in zip(range(N), self.movies):
-                #update item bias b_n(v)
-                rating_sum = 0.0
-                for m, rating in movie.ratings: 
-                    rating_sum += rating -  self.U[m,:].dot(self.V[n,:].T) - self.user_biases[m]
-                rating_sum *= self.lam
-                denom = self.gamma + self.lam*N
-                self.movie_biases[n] = rating_sum/denom
-
-                #update item embedding v_n
-                rating_sum = np.zeros((self.k,1))
-                denom_matrix = np.zeros((self.k, self.k))
-                for n, rating in user.ratings:
-                    scalar = (rating - self.user_biases[m] - self.movie_biases[n])
-                    vector = scalar*self.U[m,:].reshape((self.k, 1))
-                    rating_sum = rating_sum + vector
-                    denom_matrix += self.U[m,:].dot(self.U[m,:])
-
-                rating_sum = rating_sum*self.lam
-                denom_matrix = denom_matrix*self.lam + self.tau*np.eye(self.k)
-                denom_matrix = np.linalg.inv(denom_matrix)
-
-                self.V[n,:] = denom_matrix.dot(rating_sum.reshape((self.k,1))).reshape((self.k,))
-
-            it += 1
-
-            RMSE.append(self.RMSE())
-            neg_log_likelihoods.append(self.neg_log_likelihood())
-
-        return neg_log_likelihoods, RMSE
+        
+        return np.sqrt(sum_rankings/num_ratings)
 
     def update_user_bias(lam, gamma, ratings, user_embedding, item_embeddings, item_biases):
         coeff = lam/(gamma + lam*ratings.shape[0])
@@ -226,22 +152,29 @@ class Recommender:
     def update_user_embedding(lam, tau, V, ratings, user_bias, item_biases):
         N, k = V.shape
 
-        item_indices = ratings[:,0].astype(np.int32)
-        r_vec = ratings[:,1].reshape((1, -1))
-        item_biases_vec = item_biases[item_indices].reshape((1, -1))
+        item_indices = ratings[:,0].astype(int)
+        r_vec = ratings[:,1].reshape((-1, 1))
+        item_biases_vec = item_biases[item_indices].reshape((-1, 1))
 
         vec = r_vec - item_biases_vec - user_bias
 
         #sum should be be a k dimensional vector
-        num_sum = np.dot(vec, V[item_indices, :])*lam
+        num_sum = lam*(V[item_indices, :].T @ vec)
 
-        matrix = lam*np.sum([np.outer(row, row) for row in V[item_indices, :]]) + tau*np.eye(k)
+        outer_matrix = np.zeros((k, k))
+        for row in V[item_indices, :]:
+            outer_matrix += np.outer(row, row)
 
-        return (np.linalg.inv(matrix) @ num_sum.reshape((-1,1))).reshape((k,))
+        matrix = lam*outer_matrix + tau*np.eye(k)
 
+        return np.linalg.solve(matrix, num_sum.reshape((-1,1))).reshape((k,))
 
     def update_item_embedding(lam, tau, user_embeddings, ratings, item_bias, user_biases):
         pass
+
+    def avg_inner_product(matrix):
+        inner_products = np.einsum('ij,ij->i', matrix, matrix)
+        return np.mean(inner_products)
 
     def fit_vectorized(self, max_iter=20):
         M = len(self.users)
@@ -253,12 +186,44 @@ class Recommender:
         self.movie_biases = np.zeros((N))
 
         #initialize U and V matrices
-        self.U = np.zeros((M, self.k))
-        self.V = np.zeros((N, self.k))
-        print(f"V: {len(self.V)}")
+        #self.U = np.zeros((M, self.k))
+        #self.V = np.zeros((N, self.k))
+        self.U = np.random.normal(0, 1/np.sqrt(self.k), size=(M, self.k))
+        self.V = np.random.normal(0, 1/np.sqrt(self.k), size=(N, self.k))
 
-        neg_log_liks = []
-        RMSEs = []
+        statistics = {} 
+        statistics['neg_log_liks'] = []
+        statistics['RMSEs'] = []
+        statistics['user_embed_length'] = []
+        statistics['item_embed_length'] = []
+        statistics['mean_user_bias'] = []
+        statistics['mean_item_bias'] = []
+
+        statistics['neg_log_liks'].append(self.neg_log_likelihood())
+        statistics['RMSEs'].append(self.RMSE())
+        statistics['user_embed_length'].append(Recommender.avg_inner_product(self.U))
+        statistics['item_embed_length'].append(Recommender.avg_inner_product(self.V))
+        statistics['mean_user_bias'].append(np.mean(self.user_biases))
+        statistics['mean_item_bias'].append(np.mean(self.movie_biases))
+
+        #initial update to user and item biases
+        #update user parameters
+        for m, user in zip(range(M), self.users):
+            ratings = user.ratings
+            #---user biases---
+            self.user_biases[m] = Recommender.update_user_bias(self.lam, self.gamma, ratings, np.zeros((self.k, )), np.zeros_like(self.V), self.movie_biases)
+
+        for n, item in zip(range(N), self.movies):
+            ratings = item.ratings
+            #---item biases---
+            self.movie_biases[n] = Recommender.update_item_bias(self.lam, self.gamma, ratings, np.zeros((self.k, )), np.zeros_like(self.U), self.user_biases)
+        
+        statistics['neg_log_liks'].append(self.neg_log_likelihood())
+        statistics['RMSEs'].append(self.RMSE())
+        statistics['user_embed_length'].append(Recommender.avg_inner_product(self.U))
+        statistics['item_embed_length'].append(Recommender.avg_inner_product(self.V))
+        statistics['mean_user_bias'].append(np.mean(self.user_biases))
+        statistics['mean_item_bias'].append(np.mean(self.movie_biases))
 
         for it in tqdm(range(max_iter)):
             #update user parameters
@@ -271,8 +236,6 @@ class Recommender:
                 user_embedding = Recommender.update_user_embedding(self.lam, self.tau, self.V, ratings, self.user_biases[m], self.movie_biases)
                 self.U[m,:] = user_embedding
             
-            print("Finished with users")
-                
             #update movie parameters
             for n, item in zip(range(N), self.movies):
                 ratings = item.ratings
@@ -283,7 +246,17 @@ class Recommender:
                 item_embedding = Recommender.update_user_embedding(self.lam, self.tau, self.U, ratings, self.movie_biases[n], self.user_biases)
                 self.V[n, :] = item_embedding
             
-            RMSEs.append(self.RMSE())
-            neg_log_liks.append(self.neg_log_likelihood())
+            statistics['neg_log_liks'].append(self.neg_log_likelihood())
+            statistics['RMSEs'].append(self.RMSE())
+            statistics['user_embed_length'].append(Recommender.avg_inner_product(self.U))
+            statistics['item_embed_length'].append(Recommender.avg_inner_product(self.V))
+            statistics['mean_user_bias'].append(np.mean(self.user_biases))
+            statistics['mean_item_bias'].append(np.mean(self.movie_biases))
 
-        return RMSEs, neg_log_liks
+        return statistics 
+
+def run_cmdline():
+    rec = Recommender(lam=0.01, gamma=0.01, tau=0.01)
+    rec.initialize_from_csv("ml-latest-small/movies.csv", "ml-latest-small/ratings.csv")
+    print(len(rec.users))
+    print(len(rec.movies))
